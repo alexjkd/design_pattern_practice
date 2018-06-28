c2b6725cdc847a92f4e980a5ba5eb1c3b484f780
Melon Bao <melon.bao@outlook.com>
2018-06-28 15:59:32 +0800

    add for the singleton pattern

---
+++ singleton/DataBaseConnection.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Melon Bao
+ * Date: 2018/6/28
+ * Time: 15:33
+ */
+
+namespace dataBaseConnection;
+
+
+class dataBaseConnection
+{
+    private static $conn;
+
+    private function __construct()
+    {
+        $this->conn= mysqli_connect('localhost','root','');
+        printf("data base connection has been created<br/>");
+    }
+
+    //return obj is a mysql object
+    public static function getInstance()
+    {
+        if(!(self::$conn instanceof self))
+        {
+            self::$conn = new self;
+        }
+    }
+
+    public function __clone()
+    {
+        // TODO: Implement __clone() method.
+        trigger_error("Clone is not allowed!");
+    }
+}
+
+printf("this is the first connection<br/>");
+$mysqlConnection = dataBaseConnection::getInstance();
+printf("this is the second connection<br/>");
+$mysqlConnection2 = $mysqlConnection;
