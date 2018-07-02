57b36d9f65c62b095262b5c79f11e8a10a93405e
Melon Bao <melon.bao@outlook.com>
2018-07-02 21:37:01 +0800

    [factory]: Add file for abstract factory pattern

---
+++ Factory/Interfaces.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 2018/7/2
+ * Time: 7:25 PM
+ */
+
+Interface IIngredientFactory
+{
+    public function createDough();
+    public function createSauce();
+    public function createVeggies();
+    public function createPepperoni();
+    public function createClans();
+}
+abstract class Pizza
+{
+    protected $Dough;
+    protected $Sauce;
+    protected $Veggies;
+    protected $Pepperoni;
+    protected $Clans;
+    protected $ingredientFactory;
+
+    public abstract function prepare();
+
+    public function __construct($ingredientFactory)
+    {
+        $this->ingredientFactory = $ingredientFactory;
+    }
+
+    public function getClans()
+    {
+        return $this->Clans;
+    }
+
+    public function bake()
+    {
+        printf("baking the pizza...\n");
+    }
+
+    public function cut()
+    {
+        printf("cutting the pizza...\n");
+    }
+
+    public function box()
+    {
+        printf("boxing the pizza...");
+    }
+}
+
+class ClamPizza extends Pizza
+{
+    public function prepare()
+    {
+        // TODO: Implement prepare() method.
+        $this->Clans = $this->ingredientFactory->createClans();
+    }
+}
+
+class DoughPizza extends Pizza
+{
+    public function prepare()
+    {
+        // TODO: Implement prepare() method.
+    }
+}
+
+
+abstract class PizzaStore
+{
+    protected abstract function createPizza($type);
+
+    private $Pizza;
+    public function order($type)
+    {
+        $this->Pizza = $this->createPizza($type);
+        $this->Pizza->prepare();
+        $this->Pizza->bake();
+        $this->Pizza->cut();
+        $this->Pizza->box();
+    }
+
+    public function getPizza()
+    {
+        return $this->Pizza;
+    }
+}
+
---
+++ Factory/NYPizzaStore.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 2018/7/2
+ * Time: 8:20 PM
+ */
+require_once 'Interfaces.php';
+
+class NYIngredientFactory implements IIngredientFactory
+{
+    public function createClans():int
+    {
+        printf("This is NY style with Clans for Pizza\n");
+        return 10;
+    }
+    public function createDough()
+    {
+        // TODO: Implement createDough() method.
+    }
+    public function createPepperoni()
+    {
+        // TODO: Implement createPepperoni() method.
+    }
+    public function createSauce()
+    {
+        // TODO: Implement createSauce() method.
+    }
+    public function createVeggies()
+    {
+        // TODO: Implement createVeggies() method.
+    }
+}
+
+class NYPizzaStore extends PizzaStore
+{
+    protected function createPizza($type)
+    {
+        $ingredientFactory = new NYIngredientFactory();
+        // TODO: Implement createPizza() method.
+        if($type === 'Clam')
+        {
+            return new ClamPizza($ingredientFactory);
+        }
+    }
+}
